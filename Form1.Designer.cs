f8dc52a9b3a9747f5290cea82161439b909dff81
attanai <andrew.aeontek@gmail.com>
2015-02-20 09:02:40 -0600

    First upload
    First upload of various projects.

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(2, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(774, 61);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "This program will replace text in any of the accepted file formats, including .do" +
+    "c, .docx, .xlsx, and any format that can be opened by notepad. To do all files i" +
+    "n a directory, select \"Batch\".";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select File";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(655, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(676, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Text to Find";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 164);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(652, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Replace with";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 213);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(652, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 72);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Batch";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(676, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Replace";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ".docx"});
+            this.comboBox1.Location = new System.Drawing.Point(546, 137);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.Visible = false;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(490, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "File Type";
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 254);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Replace";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string file = "";
+        string find = "";
+        string replaceWith = "";
+        List<string> repAll = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "This program will replace text in any .docx file or any format that can be opened by notepad.\r\nTo do all files in a directory, select \"Batch\".";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.openFileDialog1.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox2.Text = this.openFileDialog1.FileName;
+            }
+        }
+
+        private void button1_Click1(object sender, EventArgs e)
+        {
+            DialogResult result = this.folderBrowserDialog1.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox2.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            file = textBox2.Text;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                label2.Text = "Select Folder";
+                label4.Visible = true;
+                comboBox1.Visible = true;
+                button1.Click += new EventHandler(button1_Click1);
+                button1.Click -= new EventHandler(button1_Click);
+            }
+            else if (checkBox1.Checked != true)
+            {
+                label2.Text = "Select File";
+                label4.Visible = false;
+                comboBox1.Visible = false;
+                button1.Click += new EventHandler(button1_Click);
+                button1.Click -= new EventHandler(button1_Click1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                if (checkBox1.Checked == true)
+                {
+                    file = pathTrim(file);
+                    if (Directory.Exists(file) == true)
+                    {
+                        string ext = extTrim(comboBox1.Text);
+
+                        string[] filePaths = Directory.GetFiles(@file, "*" + ext);
+                        if (filePaths == null)
+                        {
+                            MessageBox.Show("There are no files with that extension.\r\n(Format must be \".abc\" with only alphanumeric numbers");
+                        }
+                        else if (filePaths != null)
+                        {
+                            foreach (string blip in filePaths)
+                            {
+                                replace(blip, find, replaceWith);
+                            }
+                        }
+
+
+                    }
+                    else if (Directory.Exists(file) != true)
+                    {
+                        MessageBox.Show("This folder does not exist");
+                    }
+
+
+                }
+                else if (checkBox1.Checked != true)
+                {
+                    if (File.Exists(file) == true)
+                    {
+                        replace(file, find, replaceWith);
+
+                    }
+                    else if (File.Exists(file) != true)
+                    {
+                        MessageBox.Show("This File does not exist");
+                    }
+                }
+
+
+            }
+            MessageBox.Show("Done.");
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            find = textBox3.Text;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            replaceWith = textBox4.Text;
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private string pathTrim(string path)
+        {
+            char[] rem = { '\\', ' ' };
+            path = path.TrimEnd(rem);
+            path = path + "\\";
+            return path;
+        }
+
+        private string extTrim(string ext)
+        {
+
+            ext = Regex.Replace(ext, @"[\W]", "");
+            ext = "." + ext;
+            return ext;
+        }
+
+        private void replace(string rfile, string rfind, string rrep)
+        {
+            if (rfile.Contains("~$") != true)
+            {
+                string ext = Path.GetExtension(rfile);
+                if (ext == ".docx")
+                {
+
+                    try
+                    {
+                        string docText = "";
+                        using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(rfile, true))
+                        {
+                            using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                            {
+                                docText = sr.ReadToEnd();
+                            }
+
+                            Regex regexText = new Regex(rfind);
+                            docText = regexText.Replace(docText, rrep);
+
+
+                            using (StreamWriter sw = new StreamWriter(wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
+                            {
+                                sw.Write(docText);
+                            }
+                            foreach (FooterPart footer in wordDoc.MainDocumentPart.FooterParts)
+                            {
+                                string footerText = null;
+                                using (StreamReader sr = new StreamReader(footer.GetStream()))
+                                {
+                                    footerText = sr.ReadToEnd();
+                                }
+                                footerText = regexText.Replace(footerText, rrep);
+                                using (StreamWriter sw = new StreamWriter(footer.GetStream(FileMode.Create)))
+                                {
+                                    sw.Write(footerText);
+                                }
+                            }
+
+                            foreach (HeaderPart header in wordDoc.MainDocumentPart.HeaderParts)
+                            {
+                                string headerText = null;
+                                using (StreamReader sr = new StreamReader(header.GetStream()))
+                                {
+                                    headerText = sr.ReadToEnd();
+                                }
+
+
+                                headerText = regexText.Replace(headerText, rrep);
+
+                                using (StreamWriter sw = new StreamWriter(header.GetStream(FileMode.Create)))
+                                {
+                                    sw.Write(headerText);
+                                }
+                            }
+
+
+                        }
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show(rfile + "could not be modified");
+                    }
+                }
+
+                else if (ext != ".docx")
+                {
+                    try
+                    {
+                        string txt = File.ReadAllText(rfile);
+
+                        txt.Replace(rfind, rrep);
+
+                        File.WriteAllText(rfind, txt);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("The text could not be replaced in \"" + rfile + "\". It may not be an accepted file type.");
+                    }
+                }
+
+
+
+            }
+        }
+    }
+}
